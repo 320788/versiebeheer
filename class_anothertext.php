a0268d7e660236a3b19d4bdbfbb90a7528a88215
Igmar Nagtegaal <dedicatedtoionic@gmail.com>
2019-05-08 16:11:11 +0200

    Added a thrid class

---
+++ class_anothertext.php
@@ -0,0 +1,10 @@
+<?php
+
+    class Text2 {
+
+        var $text2 = "This is a text test";
+        var $text3 = "This is a fourth text for some reason";
+
+    }
+
+?>
--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php
 
     include("./class_text.php");
+    include("./class_anothertext.php");
     echo "Hey <br>";
     $text = new Text();
+    $textsecond = new Text2();
 
     echo $text->sometext . "<br>";
-    echo $text->secondtext;
+    echo $text->secondtext . "<br>";
+    echo $textsecond->text3;
 
 ?>
